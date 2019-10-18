dc2589515c14302385a21f767994ddb0b3f226ce
apon <aponahamed02@gmail.com>
2019-10-19 01:36:43 +0600

    final protfolio

---
+++ layouts/navbar.php
@@ -0,0 +1,49 @@
+
+<nav class="navbar navbar-default navbar-trans navbar-expand-lg fixed-top">
+    <div class="container">
+      <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarDefault"
+        aria-controls="navbarDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+      <a class="navbar-brand text-brand mr-5" href="index.php"><span class="color-a">Apon</span><span class="color-b"> Ali</span></a> 
+      <div class="search-container"style="margin-left: 100px;">
+        <form action="/action_page.php">
+          <input type="text" placeholder="Search.." name="search">
+          <button type="submit"><i class="fa fa-search"></i></button>
+        </form>
+      </div>
+      <div class="navbar-collapse collapse justify-content-center ml-5" id="navbarDefault">
+        <ul class="navbar-nav" >
+          <li class="nav-item">
+            <a class="nav-link" href="index.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#contact">Contact</a>
+          </li>
+
+          <!-- use social link -->
+
+          <li class="nav-item "style="margin-left: 80px;">
+            <a class="nav-link" href="all_profile.php"><i class="fa fa-"></i></a>
+          </li>
+          <li class="nav-item "style="margin-left: 0px;">
+            <a class="nav-link" href="all_profile.php"><i class="fa fa-facebook"></i></a>
+          </li>
+          <li class="nav-item "style="margin-left: 0px;">
+            <a class="nav-link" href="all_profile.php"><i class="fa fa-twitter"></i></a>
+          </li>
+          <li class="nav-item "style="margin-left: 0px;">
+            <a class="nav-link" href="all_profile.php"><i class="fa fa-linkedin"></i></a>
+          </li>
+          <li class="nav-item "style="margin-left: 0px;">
+            <a class="nav-link" href="all_profile.php"><i class="fa fa-instagram"></i></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+</nav>
